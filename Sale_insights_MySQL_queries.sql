6d07482a84c08542f4075b02919bfe69361703a8
tanngnle <120621197+tanngnle@users.noreply.github.com>
2022-12-27 13:45:42 +0700

    MySQL queries Used IN MySQL Workbench
    Here are the queries which would support me in building my Dashboard as well as Validating it.

--- Sale_insights_MySQL_queries.sql
+++ Sale_insights_MySQL_queries.sql
@@ -1,67 +1,41 @@
+Data Analysis Using SQL
 
--- 1. Show all customer records
-select * 
-from portfolio_sale_insights.customers;
+1. Show all customer records
 
--- 2. Show total number of customers
-select count(*) 
-from portfolio_sale_insights.customers;
+    `SELECT * FROM customers;`
 
--- 3. Show transactions for Chennai market
-select *
-from portfolio_sale_insights.transactions as t
-inner join portfolio_sale_insights.markets as m
-on t.market_code = m.markets_code
-where m.markets_name = 'Chennai';
+2. Show total number of customers
 
--- 4. Show transactions for Chennai market (market code for chennai is Mark001)
-select *
-from portfolio_sale_insights.transactions
-where market_code = 'Mark001';
+    `SELECT count(*) FROM customers;`
 
--- 5. Show distinct product codes that were sold in chennai
-select distinct product_code
-from portfolio_sale_insights.transactions
-where market_code = 'Mark001';
+3. Show transactions for Chennai market (market code for chennai is Mark001
 
--- 6. Show transactions where currency is US dollars
-select *
-from portfolio_sale_insights.transactions
-where currency = 'USD';
+    `SELECT * FROM transactions where market_code='Mark001';`
 
--- 7. Show market name where currency is US dollars
-select distinct markets_name, currency
-from portfolio_sale_insights.transactions as t
-inner join portfolio_sale_insights.markets as m
-on t.market_code = m.markets_code
-where currency = 'USD';
+4. Show distrinct product codes that were sold in chennai
 
--- 8. Show transactions in 2020 join by date table
-select t.*
-from portfolio_sale_insights.transactions as t
-inner join portfolio_sale_insights.date as d
-on t.order_date = d.date
-where d.year = 2020;
+    `SELECT distinct product_code FROM transactions where market_code='Mark001';`
 
--- 9. Show total revenue in year 2020
-select sum(sales_amount)
-from portfolio_sale_insights.transactions as t
-inner join portfolio_sale_insights.date as d
-on t.order_date = d.date
-where d.year = 2020;
+5. Show transactions where currency is US dollars
 
--- 10. Show total revenue in year 2020, January Month
-select sum(sales_amount)
-from portfolio_sale_insights.transactions as t
-inner join portfolio_sale_insights.date as d
-on t.order_date = d.date
-where d.year = 2020
-and d.month_name = 'January';
+    `SELECT * from transactions where currency="USD"`
 
--- 11. Show total revenue in year 2020 in Chennai
-select sum(sales_amount)
-from portfolio_sale_insights.transactions as t
-inner join portfolio_sale_insights.date as d
-on t.order_date = d.date
-where d.year = 2020
-and market_code = 'Mark001';
+6. Show transactions in 2020 join by date table
+
+    `SELECT transactions.*, date.* FROM transactions INNER JOIN date ON transactions.order_date=date.date where date.year=2020;`
+
+7. Show total revenue in year 2020,
+
+    `SELECT SUM(transactions.sales_amount) FROM transactions INNER JOIN date ON transactions.order_date=date.date where date.year=2020 and transactions.currency="INR\r" or transactions.currency="USD\r";`
+	
+8. Show total revenue in year 2020, January Month,
+
+    `SELECT SUM(transactions.sales_amount) FROM transactions INNER JOIN date ON transactions.order_date=date.date where date.year=2020 and and date.month_name="January" and (transactions.currency="INR\r" or transactions.currency="USD\r");`
+
+9. Show total revenue in year 2020 in Chennai
+
+    `SELECT SUM(transactions.sales_amount) FROM transactions INNER JOIN date ON transactions.order_date=date.date where date.year=2020
+and transactions.market_code="Mark001";`
+
+
+
